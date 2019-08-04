8bc280ddd021578be1e85bac7773e0db616e7347
mariebell <sugary.macarons@gmail.com>
2019-08-04 15:32:13 +0900

    gray balloons

--- main.js
+++ main.js
@@ -5,16 +5,24 @@ clickElement.addEventListener("click", function() {
   location.href = "https://thom-i.github.io/aframe/030/sample-030/";
 },false);
 
-var scoreBoard = document.querySelector('#scoreBoard');
+// var scoreBoard = document.querySelector('#scoreBoard');
 
-var crushBalloon = document.getElementsByClassName('countup');
-for(let i=0; i < crushBalloon.length; i++) {
-  crushBalloon[i].addEventListener('click', () => {
-    scoreBoard.innerHTML = ++score;
-    crushBalloon[i].parentNode.removeChild(crushBalloon[i]);
-  }, false);
-}
+// var crushBalloon = document.getElementsByClassName('countup');
+// for(let i=0; i < crushBalloon.length; i++) {
+//   crushBalloon[i].addEventListener('click', () => {
+//     alert(i);
+//     scoreBoard.innerHTML = ++score;
+//     crushBalloon[i].parentNode.removeChild(crushBalloon[i]);
+//   }, false);
+// }
 
-function emitBalloon() {
-  
-}
+
+$(function() {
+  var jcrushBalloon = $('.countup');
+  jcrushBalloon.each(function() {
+    $(this).on('click', function() {
+      scoreBoard.innerHTML = ++score;
+      $(this).remove();
+    });
+  });
+});
